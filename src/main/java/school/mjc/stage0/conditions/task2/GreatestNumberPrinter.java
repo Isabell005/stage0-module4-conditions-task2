100206307b71b83d0589e983e3148e4b2f6a0686
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-13 13:16:52 +0400

    stage0Module4ConditionsTask2

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,8 +2,7 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
-        first = 12;
-        second = 1;
+
         if (first > second) {
             System.out.println(first);
         } else if (first == second) {
